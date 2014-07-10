177ba20667961e5c974457581d079862a1565abe
Harini Manivannan <harini.manivannan@gmail.com>
2014-07-10 08:34:07 +0100

    added js, css, html to make slide panel work in Browse

--- js/home-sidebar.js
+++ js/home-sidebar.js
@@ -1,6 +1,22 @@
-$(document).ready(function(){
-$('[data-slidepanel]').slidepanel({
-        orientation: 'top',
-        mode: 'push'
-    });
+ $(document).ready(function(){
+
+//$('[data-slidepanel]').slidepanel({
+//    orientation: 'top',
+//     mode: 'push'
+//   });
+
+
+// $('.collapse').collapse('hide');
+
+  $('#slide-panel').on('click', function() {
+    var panel = $('#home-sidebar');
+    if (panel.hasClass("visible")) {
+      panel.removeClass('visible').animate({'margin-left':'-300px'});
+    } else {
+      panel.addClass('visible').animate({'margin-left':'0px'});
+    }
+    return false;
+  });
 });
+
+//Do display inline for the sidebar panel to move it next to the other panel.
